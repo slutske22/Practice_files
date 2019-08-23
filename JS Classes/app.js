9b7dcc24122a08be7bdfee6bb1f62ecca39dc9e0
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-08-23 13:34:12 -0700

    pracitce

--- JS Classes/app.js
+++ JS Classes/app.js
@@ -1,3 +1,17 @@
+let mixin = {
+   madeIn(){
+      console.log('This car was made in Korea')
+   }
+}
+
+let carMixIn = {
+   __proto__: mixin,
+   madeIn(){
+      super.madeIn();
+   }
+}
+
+
 class Car{
    constructor(name, doors, engine, color){
       this.name = name;
@@ -16,11 +30,30 @@ class Car{
 }
 
 
+class SUV extends Car{
+   constructor(name, doors, engine, color, make, year, carStats){
+      super(name, doors, engine, color, carStats);
+      this.make = make;
+      this.year = year
+      this.wheels = 4;
+      this.AC = true;
+
+      // assign mixin
+      Object.assign(this, carMixIn);
+   }
+   myBrand(){
+      return console.log(`This SUV is a ${this.color} ${this.year} ${this.make}`)
+   }
+}
+
+
 const cx5 = new Car('CX5' ,2 ,'v6','blue');
-const santaFe = new Car('Santa Fe', 4,'v4','green');
+const santaFe = new SUV('Santa Fe', 4,'v4','green','Hyundai',2002);
 
 console.log(cx5);
 // console.log(cx5.carStats());
-// console.log(santaFe);
+console.log(santaFe);
 // console.log(santaFe.carStats());
 // console.log(Car.totalDoors(cx5,santaFe));
+
+console.log(santaFe.madeIn())
---
+++ JS Classes/reactkindof.js
@@ -0,0 +1,12 @@
+import React, { component } from 'react';
+
+class Button extends component{
+   render(){
+      return(
+         <button>Click Me</button>
+      )
+   }
+}
+
+
+export default Button;
