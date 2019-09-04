21b12d4d6ebf2475858541b506a06a596b5a74d7
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-09-04 16:51:46 -0700

    Update script.js

--- Firestarter Experiments/Popup Testing/script.js
+++ Firestarter Experiments/Popup Testing/script.js
@@ -20,6 +20,10 @@ var baseLayer =  new L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{
 
 baseLayer.addTo(leafletMap)
 
+
+
+
+
 // Creating the nodes to be added
 // var removeMeNode = document.createElement("a");
 // var removeMeText = document.createTextNode("Remove this marker");
@@ -44,21 +48,55 @@ L.Marker.include({
    allowRemoval: function(){
 
 
-         myContainer = this.getPopup()._wrapper;
-         let templateRemoveMe = L.DomUtil.create('a', 'popupMod remove', myContainer)
-         templateRemoveMe.innerHTML = "Remove this marker";
-         let removeButton = L.DomUtil.get(templateRemoveMe)
+         // myContainer = this.getPopup()._wrapper;
+         // let templateRemoveMe = L.DomUtil.create('a', 'popupMod remove', myContainer)
+         // templateRemoveMe.innerHTML = "Remove this marker";
+         // let removeButton = L.DomUtil.get(templateRemoveMe)
+         //
+         // let thisStandIn = this;
+         //
+         // removeButton.addEventListener("click", function(){
+         //    thisStandIn.remove(leafletMap)
+         // }, false)
 
-         let thisStandIn = this;
+         this.on("click", function(e){
+            console.log( 'e.target :' );
+            console.log( e.target );
+         })
 
-         removeButton.addEventListener("click", function(){
-            thisStandIn.remove(leafletMap)
-         }, false)
+         this.options.removable = true;
+
+         this.getPopup()._initLayout = this.initLayoutWithRemove;
+
+         // https://github.com/Leaflet/Leaflet/blob/master/src/layer/Popup.js
+
+   },
 
 
+   initLayoutWithRemove: function () {
+      var prefix = 'leaflet-popup',
+          container = this._container = L.DomUtil.create('div',
+         prefix + ' ' + (this.options.className || '') +
+         ' leaflet-zoom-animated');
 
+      var wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper', container);
+      this._contentNode = L.DomUtil.create('div', prefix + '-content', wrapper);
 
-   }
+      L.DomEvent.disableClickPropagation(wrapper);
+      L.DomEvent.disableScrollPropagation(this._contentNode);
+      L.DomEvent.on(wrapper, 'contextmenu', L.DomEvent.stopPropagation);
+
+      this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container', container);
+      this._tip = L.DomUtil.create('div', prefix + '-tip', this._tipContainer);
+
+      if (this.options.closeButton) {
+         var closeButton = this._closeButton = L.DomUtil.create('a', prefix + '-close-button', container);
+         closeButton.href = '#close';
+         closeButton.innerHTML = '&#215;';
+
+         L.DomEvent.on(closeButton, 'click', this._onCloseButtonClick, this);
+      }
+   },
 
 
 })
@@ -86,3 +124,6 @@ var anotherMarker =  L.marker( [33.270, -116] );
 anotherMarker
    .addTo(leafletMap)
    .bindPopup(anotherMarkerPopup)
+
+var thirdMarker = L.marker( [33.270, -115.5] )
+   .addTo(leafletMap)
