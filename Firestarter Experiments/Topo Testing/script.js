15b5c1a89012cb6bcaa18fb5d6aa415fe852c35f
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-09-13 10:26:21 -0700

    stuff

--- Firestarter Experiments/Topo Testing/script.js
+++ Firestarter Experiments/Topo Testing/script.js
@@ -272,6 +272,47 @@ var thirdMarker = L.marker( [33.270, -115.5] )
 
 
 
+   //------------------------------------------------------------------------//
+   //                RANDOM MARKER
+   //------------------------------------------------------------------------//
+
+   function randomNumber(min, max){
+      return ( Math.random() * (max - min) ) + min
+   };
+
+
+   randomMarkerButton = document.querySelector('#randomMarker');
+
+   randomMarkerButton.addEventListener("click", function(){
+      let mapBoundLeft = leafletMap.getBounds()._southWest.lng;
+      let mapBoundRight = leafletMap.getBounds()._northEast.lng;
+      let mapBoundTop = leafletMap.getBounds()._northEast.lat;
+      let mapBoundBottom = leafletMap.getBounds()._southWest.lat;
+
+
+      let randomLng = randomNumber(mapBoundLeft, mapBoundRight);
+      let randomLat = randomNumber(mapBoundTop, mapBoundBottom);
+
+      let randomCircle = L.circleMarker( [randomLat, randomLng], {
+         color: 'red',
+         weight: 0.5,
+   	   fillColor: '#f03',
+   	   fillOpacity: 0.5,
+   	   radius: 10
+      })
+         .addTo(leafletMap)
+         .bindPopup(`This is a randomly placed marker<br>
+            Latitude: ${randomLat}<br>
+            Longitude: ${randomLng}` , {removable: true, editable: true})
+         .openPopup();
+
+      leafletMap.panTo([randomLat, randomLng])
+
+   })
+
+
+
+
 /*-----------------------------------------------------------------------
 ███████ ██      ███████ ██    ██  █████  ████████ ██  ██████  ███    ██
 ██      ██      ██      ██    ██ ██   ██    ██    ██ ██    ██ ████   ██
@@ -286,6 +327,61 @@ var thirdMarker = L.marker( [33.270, -115.5] )
    ██    ███████ ███████    ██    ███████
 ------------------------------------------------------------------------*/
 
+
+//--------------  USING color picker -----------------------------------//
+
+// First load colorPicker layer using the mapbox topo RGB tileset
+var colorPicker = L.tileLayer.colorPicker('https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.outdoors',
+      accessToken: 'pk.eyJ1Ijoic2x1dHNrZTIyIiwiYSI6ImNqeGw1Y3BibDAybG4zeHFyaXl3OXVwZXUifQ.fZ_5Raq5z-DUpo2AK-bQHA'
+         //accesstoken from mapbox.com account
+         //mapbox.com username: slutske22
+         //mapbox.com PW: ורדינה1!
+      }).addTo(leafletMap);
+
+// Write some code which utilizes the getColor function, pulling data from the colorPicker layer
+leafletMap.on('click', function(event){
+   var color = colorPicker.getColor(event.latlng);
+   console.log(color);
+})
+
+
+var mapBoxOutdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.outdoors',
+      accessToken: 'pk.eyJ1Ijoic2x1dHNrZTIyIiwiYSI6ImNqeGw1Y3BibDAybG4zeHFyaXl3OXVwZXUifQ.fZ_5Raq5z-DUpo2AK-bQHA'
+      }).addTo(leafletMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------  USING CANVAS MODE -------------------------------------//
+//                no idea what im doing here lol                         //
 
 ///  from https://labs.mapbox.com/bites/00307/?elev=10#8/37.727/-123.975
 
