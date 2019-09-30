23947b912caf99437c5c3c71dd3e0bd8e6b77fc5
slutske22 <seth.lutske@gmail.com>
2019-09-29 22:57:40 -1000

    sldkasd

--- Firestarter Experiments/Active Area/script.js
+++ Firestarter Experiments/Active Area/script.js
@@ -8,14 +8,12 @@ var mapOptions = {
 var map = L.map('mapid', mapOptions);
 
 // use only active activearea
-// map.setActiveArea('activeArea');
+map.setActiveArea('activeArea');
 
 //add a baseLayer
-var baseLayer =  new L.tileLayer('https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token={accessToken}', {
+var mapBoxOutdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.outdoors',
       accessToken: 'pk.eyJ1Ijoic2x1dHNrZTIyIiwiYSI6ImNqeGw1Y3BibDAybG4zeHFyaXl3OXVwZXUifQ.fZ_5Raq5z-DUpo2AK-bQHA'
-      });
-
-baseLayer.addTo(map);
+   }).addTo(map);
