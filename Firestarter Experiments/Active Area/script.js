25127c0f3b4d6dacf26287a6795a5955a7a1838d
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-10-01 15:01:38 -0700

    jesus
    making a jsfiddle to post my question on stack exchange.  still geting the same wierd autopan problem

--- Firestarter Experiments/Active Area/script.js
+++ Firestarter Experiments/Active Area/script.js
@@ -23,67 +23,65 @@ var mapBoxOutdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{
 
 
 
-   //------------------------------------------------------------------------//
-   //                RANDOM MARKER
-   //------------------------------------------------------------------------//
+//------------------------------------------------------------------------//
+//                RANDOM MARKER
+//------------------------------------------------------------------------//
 
-   // Make empty array to recieve randomly generated markers
+// Make empty array to recieve randomly generated markers
+randomMarkerArray = [];
+
+// Useful random number function
+function randomNumber(min, max){
+   return ( Math.random() * (max - min) ) + min
+};
+
+// Define buttons
+var randomMarkerButton = document.querySelector('#randomMarker');
+var clearMarkersButton = document.querySelector('#clearRandomMarkers');
+
+// Empty variable to recieve the layergroup which is created when a thebutton is pressed
+var randomMarkerGroup;
+
+// When the button is pressed:
+randomMarkerButton.addEventListener("click", function(){
+   // Get map bounds:
+   let mapBoundLeft = map.getBounds()._southWest.lng;
+   let mapBoundRight = map.getBounds()._northEast.lng;
+   let mapBoundTop = map.getBounds()._northEast.lat;
+   let mapBoundBottom = map.getBounds()._southWest.lat;
+
+   // Make random number within bounds
+   let randomLng = randomNumber(mapBoundLeft, mapBoundRight);
+   let randomLat = randomNumber(mapBoundTop, mapBoundBottom);
+
+   let position = [randomLat, randomLng];
+
+   // Create a random marker
+   let randomMarker = L.marker( position )
+      .bindPopup(`This is a randomly placed marker<br>
+         <br>
+         Latitude: ${randomLat}<br>
+         Longitude: ${randomLng}` , {removable: true, editable: true});
+
+   // map.panTo(position)
+
+   // Give each random marker its own name with its index for easy reference
+   randomMarker._name = `Random Marker ${randomMarkerArray.length + 1}`;
+   // Push it into the array
+   randomMarkerArray.push(randomMarker);
+
+   // Create layerGroup using the array
+   randomMarkerGroup = L.layerGroup( randomMarkerArray )
+      .addTo(map);
+   // Open popup on most recently added marker
+   randomMarkerArray[randomMarkerArray.length - 1].openPopup();
+
+})
+
+
+clearMarkersButton.addEventListener("click", function(){
+   // Remove the layergroup of random markers
+   randomMarkerGroup.remove();
+   // Empty the array to truly refresh the random marker layer
    randomMarkerArray = [];
-
-   // Useful random number function
-   function randomNumber(min, max){
-      return ( Math.random() * (max - min) ) + min
-   };
-
-   // Define buttons
-   var randomMarkerButton = document.querySelector('#randomMarker');
-   var clearMarkersButton = document.querySelector('#clearRandomMarkers');
-
-   // Empty variable to recieve the layergroup which is created when a the button is pressed
-   var randomMarkerGroup;
-
-   // When the button is pressed:
-   randomMarkerButton.addEventListener("click", function(){
-      // Get map bounds:
-      let mapBoundLeft = leafletMap.getBounds()._southWest.lng;
-      let mapBoundRight = leafletMap.getBounds()._northEast.lng;
-      let mapBoundTop = leafletMap.getBounds()._northEast.lat;
-      let mapBoundBottom = leafletMap.getBounds()._southWest.lat;
-
-      // Make random number within bounds
-      let randomLng = randomNumber(mapBoundLeft, mapBoundRight);
-      let randomLat = randomNumber(mapBoundTop, mapBoundBottom);
-
-      let position = [randomLat, randomLng];
-      let elevation = getElevation( position );
-
-      // Create a random marker
-      let randomMarker = L.marker( position )
-         .bindPopup(`This is a randomly placed marker<br>
-            <br>
-            Latitude: ${randomLat}<br>
-            Longitude: ${randomLng}<br>
-            Elevation: ${elevation}` , {removable: true, editable: true});
-
-      // leafletMap.panTo(position)
-
-      // Give each random marker its own name with its index for easy reference
-      randomMarker._name = `Random Marker ${randomMarkerArray.length + 1}`;
-      // Push it into the array
-      randomMarkerArray.push(randomMarker);
-
-      // Create layerGroup using the array
-      randomMarkerGroup = L.layerGroup( randomMarkerArray )
-         .addTo(leafletMap);
-      // Open popup on most recently added marker
-      randomMarkerArray[randomMarkerArray.length - 1].openPopup();
-
-   })
-
-
-   clearMarkersButton.addEventListener("click", function(){
-      // Remove the layergroup of random markers
-      randomMarkerGroup.remove();
-      // Empty the array to truly refresh the random marker layer
-      randomMarkerArray = [];
-   }, false);
+}, false);
