0f63dbe9fb641f91878926951f1ff834f57a91f3
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-10-01 16:30:49 -0700

    Update script.js
    Weather call workinnnggg bittcheessss

--- API Practice/OpenWeatherMap Practice/script.js
+++ API Practice/OpenWeatherMap Practice/script.js
@@ -2,15 +2,14 @@
 var request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'api.openweathermap.org/data/2.5/weather?lat=35&lon=139', true);
+request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=ae9a514eab7934500eeb71f723b38277', true);
 
 request.onload = function(){
    // Begin accessing JSON data here
    var data = JSON.parse(this.response)
 
    if (request.status >= 200 && request.status < 400) {
-
-
+      console.log(data);
    } else {
      console.log('error')
    }
