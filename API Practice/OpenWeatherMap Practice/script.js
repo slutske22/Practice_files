699a8d72215c8453ab9ad671fdc3f97bf1a201b9
slutske22 <seth.lutske@gmail.com>
2019-10-01 21:39:54 -0700

    Update script.js

--- API Practice/OpenWeatherMap Practice/script.js
+++ API Practice/OpenWeatherMap Practice/script.js
@@ -1,8 +1,11 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 
+var openWeatherMapsApiKey = 'ae9a514eab7934500eeb71f723b38277';
+var url = `https://api.openweathermap.org/data/2.5/weather?lat=33&lon=-116&APPID=${openWeatherMapsApiKey}`
+
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=33&lon=-116&APPID=ae9a514eab7934500eeb71f723b38277', true);
+request.open('GET', url, true);
 
 request.onload = function(){
    // Begin accessing JSON data here
