5e5118f6fbe44de2882c8dd8cbf9491c6d292c86
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-10-25 15:31:22 -0700

    Update test.js

--- Promises/test.js
+++ Promises/test.js
@@ -27,8 +27,8 @@ function get(url) {
 
 
 
-const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
-//const apiKey = '';
+// const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
+const apiKey = '';
 
 const url =
   'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' +
@@ -37,5 +37,17 @@ const url =
 
 get(url)
    .then( function(data){
-      console.log(data);
+      return dataObject = JSON.parse(data);
+   })
+   .then( function(){
+      console.log(dataObject);
+   })
+   .then( function(){
+      console.log(dataObject.weather);
+   })
+   .catch( () => {
+      console.log('Could not get weather');
+   })
+   .finally( () => {
+      console.log('Weather report complete');
    })
