0d5f66469fb19b5a98f228e4d8e122fe349e58e8
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-10-30 16:07:50 -0700

    Update async.js

--- Promises/async.js
+++ Promises/async.js
@@ -1,13 +1,66 @@
-async function firstAsync(){
-   let promise = new Promise( (resolve, reject) => {
-      setTimeout( ()=>{resolve("Hello!")}, 1000)
-   });
-
-   let result = await promise;
-
-   console.log(result);
-   console.log("Message complete");
-}
+const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
+const sanDiegoUrl =
+ 'https://api.openweathermap.org/data/2.5/weather?q=san+diego&APPID=' +
+ apiKey;
 
 
-firstAsync()
+
+ //------------------------------------------------//
+ //
+ //    METHOD ONE: FUNCTION THAT RETURNS A PROMISE
+ //
+ //------------------------------------------------//
+
+function fromPromise(){
+    return new Promise((resolve, reject) => {
+       const request = new XMLHttpRequest();
+       request.open("GET", sanDiegoUrl);
+       request.onload = function(){
+          if (request.status === 200) {
+             resolve(request.response)
+          } else {
+             reject(request.statusText)
+          }
+       console.log('%c Using fromPromise()' , 'font-weight:bold');
+       console.log(request.status, request.statusText);
+       } // .onload
+       request.send()
+
+    }) // Promise
+
+} // fromPromise()
+
+
+ fromPromise().then( (data) => {
+    console.log(JSON.parse(data));
+})
+
+
+//------------------------------------------------//
+//
+//    METHOD TWO: ASYNC FUNCTION
+//
+//------------------------------------------------//
+
+async function fromAsync(){
+   let promise = new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open("GET", sanDiegoUrl);
+      request.onload = function(){
+         if (request.status === 200) {
+            resolve(request.response)
+         } else {
+            reject(request.statusText)
+         }
+      console.log('%c Using fromAsync()' , 'font-weight:bold');
+      console.log(request.status, request.statusText);
+      } // .onload
+      request.send()
+
+   }) // promise
+
+   let data = await promise;
+   console.log(JSON.parse(data));
+} // fromAsync()
+
+fromAsync();
