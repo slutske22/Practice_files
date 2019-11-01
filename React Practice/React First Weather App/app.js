e86f711cd849c87d7f2b076d1701cdf757cdea46
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-11-01 16:36:58 -0700

    filling out the beginnings of this simple app

--- React Practice/React First Weather App/app.js
+++ React Practice/React First Weather App/app.js
@@ -1,4 +1,14 @@
-//  Chemistry Dog's first React App  :p
+//----------------------------------------------------------------//
+//
+//    CHEMISTRY DOG'S FIRST REACT APP  :P
+//
+//----------------------------------------------------------------//
+
+
+
+//----------------------------------------------------------------//
+//    Generic Use functions and terms
+//----------------------------------------------------------------//
 
 // Modulus term for flying off the end of an array
 function modulus(i, n){
@@ -13,13 +23,90 @@ var date = new Date();
 
 
 
+//----------------------------------------------------------------//
+//    Openweathermaps Caller
+//----------------------------------------------------------------//
 
+var openWeatherMapsApiKey = 'ae9a514eab7934500eeb71f723b38277';
+var cityName = 'san+diego';
+// Endpoint for a 7 day forecast.  can also use `?zip=${zip}` instead of `q=${cityName},us` to get as a function of zip code
+var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},us&cnt=7&mode=json&APPID=${openWeatherMapsApiKey}`
+
+
+function getWeather(url){
+   return new Promise( (resolve, reject) => {
+      var weatherRequest = new XMLHttpRequest()
+      weatherRequest.open('GET', url);
+      weatherRequest.onload = function(){
+         if (weatherRequest.status === 200) {
+           resolve(weatherRequest.response)
+         } else {
+           reject(weatherRequest.statusText)
+         }
+      } // .onload
+      weatherRequest.send()
+   })
+}
+
+getWeather(url).then( (data) => {
+   console.log(JSON.parse(data));
+}).catch( (error) => {
+   console.log(error);
+})
+
+
+
+
+
+
+//----------------------------------------------------------------//
+//  Building the App in React
+//----------------------------------------------------------------//
 
 class App extends React.Component {
    render() {
-      return <Week />
+      return (
+         <div className="app">
+            <div className="body">
+               <Locator />
+               <Week />
+            </div>
+         </div>
+      )
    }
 }
+
+
+class Locator extends React.Component{
+   render() {
+      return(
+         <form className="locator">
+            <h2>Choose your Location</h2>
+            <Input placeholder="Search by City Name" />
+            <Input placeholder="Search by Zip" />
+         </form>
+      )
+   }
+}
+
+
+class Input extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {value: ''}
+      this.checkInput = this.checkInput.bind(this);
+   }
+
+   checkInput(e){
+      this.setState({value: e.target.value})
+      console.log(this.state.value);
+   }
+
+   render() {
+      return <input type="text" placeholder={this.props.placeholder} value={this.state.value} onChange={this.checkInput} onKeyPress={this.checkInput}></input>
+   }
+}
+
 
 class Week extends React.Component {
    renderDay(i) {
