cb1c642a73fadf49148cbe8fc2b1f4bc7d4ddbe3
slutske22 <seth.lutske@gmail.com>
2019-11-03 09:20:46 -0800

    Update app.js

--- AJAX and Fetch Class/app.js
+++ AJAX and Fetch Class/app.js
@@ -1,79 +1,83 @@
 'use strict';
 
-// https://smartystreets.com/products/apis/us-street-api
-// const smartyUrl = `https://us-street.api.smartystreets.com/street-address?auth-id=25731133668897616&candidates=10&match=invalid&street=5244%20La%20Jolla%20Mesa%20Drive&city=San%20Diego&state=CA`
+//const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=19785289899902913&candidates=10&street=86%20Frontage%20Road&city=Belmont&state=MA';
 const smartyUrl = `https://us-street.api.smartystreets.com/street-address?auth-id=25731133668897616&candidates=10`
 const parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=CA&api_key=p37gUftzQgeb61BZpCegBquiJlyyhaBubd0xpCKU`
 
-const adddressField = document.querySelector("#address");
-const cityField = document.querySelector("#city");
-const stateField = document.querySelector("#state");
-const zipField = document.querySelector("#zip");
+const addressField = document.querySelector('#address');
+const cityField = document.querySelector('#city');
+const stateField = document.querySelector('#state');
+const zipField = document.querySelector('#zip');
+
+const parkThumb = document.querySelector("#specials h2 img");
+const parkSection = document.querySelector("#specials");
+
+const parkLink = document.querySelector('#specials a');
+const parkDescription = document.querySelector('#specials p');
+
+const smartyUpdateUISuccess = function(data) {
+   const parsedData = JSON.parse(data)
+  // console.log(parsedData);
+  let zip = parsedData[0].components.zipcode
+  let plus4 = parsedData[0].components.plus4_code
+  // console.log(zip + '-' + plus4);
+  zipField.value = zip + '-' + plus4;
+};
+const smartyUpdateUIError = function(error) {
+  console.log(error);
+};
+
+const parksUpdateUISuccess = function(data){
+   parkThumb.src = 'https://www.nps.gov/common/commonspot/templates/assetsCT/images/branding/logo.png';
+   parkSection.classList.remove('hidden');
+   let parsedData = JSON.parse(data)
+   let randomNumber = Math.floor( Math.random() * parsedData.data.length)
+   console.log(randomNumber);
+   parkLink.href = parsedData.data[randomNumber].url;
+   parkLink.innerHTML = parsedData.data[randomNumber].fullName;
+   parkDescription.innerHTML = parsedData.data[randomNumber].description;
+};
+const parksUpdateUIError = function(error) {
+  console.log(error);
+};
 
 
-
-const updateUISuccess = function(data){
-   console.log(data);
+const responseMethod = function(httpRequest, succeed, fail) {
+  if (httpRequest.readyState === 4) {
+    if (httpRequest.status === 200) {
+      succeed(httpRequest.responseText);
+    } else {
+      fail(httpRequest.status + ': ' + httpRequest.responseText);
+    }
+  }
 }
 
-const updateUIError = function(error){
-   console.log(error);
-}
 
-const responseMethod = function(httpRequest){
-   if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-         updateUISuccess(httpRequest.responseText)
-      } else {
-         updateUIError(httpRequest.status + ": " + httpRequest.responseText)
-      }
+const createRequest = function(url, succeed, fail) {
+  const httpRequest = new XMLHttpRequest(url);
+  httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest, succeed, fail));
+  httpRequest.open('GET', url);
+  httpRequest.send();
+};
 
-   }
-}
-
-const createRequest = function(url){
-   const httpRequest = new XMLHttpRequest(url);
-   httpRequest.addEventListener('readystatechange', (url) => {
-      responseMethod(httpRequest);
-   })
-   httpRequest.open('GET', url);
-   httpRequest.send()
-}
-
-const checkCompletion = function(){
-   if (adddressField.value !== '' &&
+const checkCompletion = function() {
+  if (addressField.value !== '' &&
       cityField.value !== '' &&
       stateField.value !== '') {
-         const requestURL = `${smartyUrl}&street=${adddressField.value}&city=${cityField.value}&state=${stateField.value}`;
-         createRequest(requestURL)
-   }
+        const requestUrl = smartyUrl +
+          '&street=' + addressField.value +
+          '&city=' + cityField.value +
+          '&state=' + stateField.value;
+        createRequest(requestUrl, smartyUpdateUISuccess, smartyUpdateUIError);
+      }
 }
-
 // createRequest(smartyUrl);
-// createRequest(parksUrl);
+createRequest(parksUrl, parksUpdateUISuccess, parksUpdateUIError);
 
-
-adddressField.addEventListener('blur', checkCompletion);
+addressField.addEventListener('blur', checkCompletion);
 cityField.addEventListener('blur', checkCompletion);
 stateField.addEventListener('blur', checkCompletion);
 
-
-//
-//
-// (function testNP(){
-//
-//    const nationalParksAPIKey = 'p37gUftzQgeb61BZpCegBquiJlyyhaBubd0xpCKU'
-//    const nationalParksURL = `https://developer.nps.gov/api/v1/parks?stateCode=CA&api_key=${nationalParksAPIKey}`
-//
-//    let parksRequest = new XMLHttpRequest();
-//    parksRequest.open('GET', nationalParksURL)
-//    parksRequest.onload = () => {
-//       if (parksRequest.status === 200) {
-//          console.log(JSON.parse(parksRequest.responseText));
-//       } else {
-//          console.log(parksRequest.status + ": " + parksRequest.responseText);
-//       }
-//    };
-//    parksRequest.send()
-//
-// })()
+window.addEventListener('DOMContentLoaded', function(){
+   createRequest(parksUrl, parksUpdateUISuccess, parksUpdateUIError);
+}, false)
