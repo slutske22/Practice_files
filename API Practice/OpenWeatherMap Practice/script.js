cccb57ca8bea06db5e0bd9a8133cd39aea1afdc7
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-11-07 15:29:33 -0800

    Update script.js

--- API Practice/OpenWeatherMap Practice/script.js
+++ API Practice/OpenWeatherMap Practice/script.js
@@ -2,21 +2,53 @@
 var request = new XMLHttpRequest();
 
 var openWeatherMapsApiKey = 'ae9a514eab7934500eeb71f723b38277';
-var url = `https://api.openweathermap.org/data/2.5/weather?lat=33&lon=-116&APPID=${openWeatherMapsApiKey}`
+var zipCode = 90036
+var url = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&cnt=56&mode=json&APPID=${openWeatherMapsApiKey}`
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', url, true);
+// request.open('GET', url, true);
+//
+// request.onload = function(){
+//    // Begin accessing JSON data here
+//    var data = JSON.parse(this.response)
+//
+//    if (request.status >= 200 && request.status < 400) {
+//       console.log(data);
+//    } else {
+//      console.log('error')
+//    }
+// }
+//
+// // Send request
+// request.send();
 
-request.onload = function(){
-   // Begin accessing JSON data here
-   var data = JSON.parse(this.response)
 
-   if (request.status >= 200 && request.status < 400) {
-      console.log(data);
-   } else {
-     console.log('error')
-   }
+
+
+
+function getWeather(url){
+   return new Promise( (resolve, reject) => {
+      var weatherRequest = new XMLHttpRequest()
+      weatherRequest.open('GET', url);
+      weatherRequest.onload = function(){
+         if (weatherRequest.status === 200) {
+           resolve(weatherRequest.response)
+         } else {
+           reject(weatherRequest.statusText)
+         }
+      } // .onload
+      weatherRequest.send()
+   })
 }
 
-// Send request
-request.send();
+getWeather(url)
+   .then( (response) => {
+      let data = JSON.parse(response)
+      let sampleData = [ ]
+      console.log(data);
+      data.list.forEach( (hour) => {
+
+         sampleData.push( [hour.dt_txt, hour.weather[0].icon] )
+      })
+      console.log(sampleData);
+   })
