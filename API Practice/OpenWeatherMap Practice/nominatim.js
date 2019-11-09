91fc67d9d06609ab0d4890ffa0461a99168c39a4
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-11-08 16:53:37 -0800

    Update nominatim.js

--- API Practice/OpenWeatherMap Practice/nominatim.js
+++ API Practice/OpenWeatherMap Practice/nominatim.js
@@ -1,16 +1,23 @@
 // Apparently Nominatim can return a lat lng based on a zip, cityname, or whatever address.  If this works I can feed it into Dark Sky API and get better weather data than OWM gives
 
+// another option for 7-day forecast using zip or city name:
+// https://developer.here.com/api-explorer/rest/auto_weather/weather-forecast-7days-simple
+
 //https://nominatim.org/release-docs/develop/api/Search/
 
 var url = 'https://nominatim.openstreetmap.org/90036?format=json';
 
-var searchTerm = encodeURIComponent('la brea tar pits')
+var searchTerm = encodeURIComponent('san diego california');
+var cityName = encodeURIComponent('santa cruz');
+var stateName = encodeURIComponent('');
 var zipCode = 92109;
 
 var openSearchURL = `https://nominatim.openstreetmap.org/search?q=${searchTerm}&format=json`;
 
 // Zip assumes country is USA, but that can be changed
 var zipURL = `https://nominatim.openstreetmap.org/search?postalcode=${zipCode}&country=USA&format=json`;
+
+var cityURL = `https://nominatim.openstreetmap.org/search?city=${cityName}&state=${stateName}&country=USA&format=json`;
 
 function getLocationInfo(url){
    return new Promise( (resolve, reject) => {
@@ -27,7 +34,7 @@ function getLocationInfo(url){
    })
 }
 
-getLocationInfo(zipURL)
+getLocationInfo(cityURL)
    .then( (data) => {
       console.log(JSON.parse(data));
    })
