430d2e09b16af44c6e042f8b68a1d9c000da68cc
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-11-13 10:56:11 -0800

    Update parentchild.js

--- React Practice/React Intro/parentchild.js
+++ React Practice/React Intro/parentchild.js
@@ -19,11 +19,21 @@ class Parent extends React.Component{
       })
    }
 
+   showTalkBack = (childTalk) => {
+      this.setState({
+         childSaid: childTalk
+      })
+   }
+
    render(){
       return(
          <section className="parent" style={this.style}>
             <h2>This is the parent.</h2>
-            <Child handleTalkBack={this.handleTalkBack}/>
+            <p>The child says: {this.state.childSaid}</p>
+            <Child
+               handleTalkBack={this.handleTalkBack}
+               showTalkBack={this.showTalkBack}
+               childTalk={this.state.childTalk} />
          </section>
 
       )
@@ -37,7 +47,6 @@ class Child extends React.Component{
 
    style = {
       width: '80%',
-      margin: 'auto',
       padding: '10px',
       backgroundColor: 'lightblue'
    }
@@ -46,12 +55,21 @@ class Child extends React.Component{
       this.props.handleTalkBack(e.target.value)
    }
 
+   onPressEnter = (e) => {
+      if (e.keyCode === 13){
+         this.props.showTalkBack(e.target.value);
+      }
+   }
+
    render(){
       return(
          <div className="child" style={this.style}>
             <h3>This is the child</h3>
             <p style={{display: 'inline'}}>I say: </p>
-            <input type="text" onChange={this.onChange} />
+            <input type="text"
+               value={this.props.childTalk}
+               onChange={this.onChange}
+               onKeyUp={this.onPressEnter}/>
          </div>
       )
    }
