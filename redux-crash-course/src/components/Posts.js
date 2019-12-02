057c72e5f3ea772446d4be064ea4c6c942cbf336
slutske22 <seth.lutske@gmail.com>
2019-12-01 19:18:27 -0800

    setting up project, basics of react

---
+++ redux-crash-course/src/components/Posts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Posts extends React.Component{
+
+  componentDidMount(){
+      console.log(123);
+   }
+
+   render () {
+      return (
+         <div>
+            <h1>Posts</h1>
+         </div>
+      )
+   }
+}
+
+export default Posts;
