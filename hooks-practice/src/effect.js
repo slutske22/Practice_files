edd8d7045114796335cc7adc82a05d0f8fdaefbb
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-12-27 16:45:08 -0800

    messing around

--- hooks-practice/src/effect.js
+++ hooks-practice/src/effect.js
@@ -2,13 +2,11 @@ import React, {useState, useEffect} from 'react';
 
 const App = () => {
 
-   const [user, getUser] = useState('');
+   const [user, getUser] = useState({name: 'not yet known'});
 
-   //  useEffect( () => {
-   //    fetch('https://jsonplaceholder.typicode.com/users/')
-   //    .then(response => response.json())
-   //    .then(json => console.log(json))
-   // } ) // useEffect
+    useEffect( () => {
+
+   } ) // useEffect
 
    // use effect runs with every render.  that means at componentDidMount, and every componentDidUpdate
 
@@ -16,16 +14,18 @@ const App = () => {
       fetch('https://jsonplaceholder.typicode.com/users/')
       .then(response => response.json())
       .then(json => {
-         getUser(json[ Math.floor(Math.random() * 10 ) ].name)
-         console.log(user)
+         getUser(json[ Math.floor(Math.random() * 10 ) ])
+         console.log(json)
       })
    }
 
    return (
       <div>
          <p>Testing effects ova heah</p>
-         <p>The user is {user}</p>
-         <button onClick={fetchUser}>Update Count</button>
+         <h3>The user is {user.name}</h3>
+         <p>Phone: {user.phone}</p>
+         <p>{user.address ? `City: ${user.address.city}` : null}</p>
+         <button onClick={fetchUser}>Update User</button>
       </div>
    )
 }
