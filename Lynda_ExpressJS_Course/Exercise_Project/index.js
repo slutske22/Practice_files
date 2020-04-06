4755873f79bdf3b83db5a7a6c1ffce151906a2eb
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-04-06 12:24:43 -0700

    Update index.js

--- Lynda_ExpressJS_Course/Exercise_Project/index.js
+++ Lynda_ExpressJS_Course/Exercise_Project/index.js
@@ -16,27 +16,62 @@ app.get('/', (request, response) =>
 
 )
 
-app.get('/item/:id', (req,res) => {
+app.get('/item/:id', (req, res, next) => {
+
+   // Middleware that pulls the data:
    console.log(req.params.id)
    let user = Number(req.params.id)
    console.log(user)
    console.log(data[user])
+   // Middleware that uses the req object
+   console.log(`Request from ${req.originalUrl}`)
+   console.log(`Request method is ${req.method}`)
+   // Everything Above is middleware
+
    res.send(data[user])
+   next()
+}, (req, res) => {
+   console.log('did you get the right data?')
 })
+
+
+app.get('/images', (req, res) => {
+   // response.send(`A put request with /item route on port ${PORT}`)
+   // res.end()
+   // res.redirect('http://www.google.com')
+   res.download('images/logo.svg')
+})
+
+
 
 app.post('/newitem', (request, response) => {
    response.send(`A post request with /newitem route on port ${PORT}`)
 })
 
-app.put('/item', (request, response) => {
-   response.send(`A put request with /item route on port ${PORT}`)
-})
 
-app.delete('/item', (request, response) => {
-   response.send(`A delete request with /item route on port ${PORT}`)
-})
+// app.put('/item', (request, response) => {
+//    response.send(`A put request with /item route on port ${PORT}`)
+// })
+
+// app.delete('/item', (request, response) => {
+//    response.send(`A delete request with /item route on port ${PORT}`)
+// })
 
 app.listen(PORT, () => {
    console.log(`Your server is running on port ${PORT}`)
    // console.log(data)
 })
+
+
+
+
+app.route('/item')
+   .get( (request, response) => {
+      response.send(`A get request with /item route on port ${PORT}`)
+   })
+   .put( (request, response) => {
+      response.send(`A put request with /item route on port ${PORT}`)
+   })
+   .delete( (request, response) => {
+      response.send(`A delete request with /item route on port ${PORT}`)
+   })
