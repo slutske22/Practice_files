82a8a7112d0a0bc15afbfb9511eabeda415b6e46
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-07-20 15:48:59 -0700

    styling feature layers tutorial

--- ArcGIS API/04_FeatureLayer_Styling/index.js
+++ ArcGIS API/04_FeatureLayer_Styling/index.js
@@ -44,12 +44,96 @@ require([
       }
    }
 
-   var trails = new FeatureLayer({
+   var trailheads = new FeatureLayer({
       url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",
       renderer: trailheadsRenderingRules,
       labelingInfo: [trailheadsLabels]
    })
 
+   map.add(trailheads)
+
+
+
+
+   var trailsRenderRule = {
+      type: "simple",
+      symbol: {
+         type: "simple-line",
+         style: "solid",
+         color: "#BA55D3"
+      },
+      visualVariables: [
+         {
+            type: "size",
+            field: "ELEV_GAIN",
+            minDataValue: 0,
+            maxDataValue: 2300,
+            minSize: "3px",
+            maxSize: "7px"
+         }
+      ]
+   }
+
+   var trails = new FeatureLayer({
+      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+      renderer: trailsRenderRule,
+      opacity: 0.75
+   })
+
    map.add(trails)
+
+   var bikeTrailsRenderingRule = {
+      type: "simple",
+      symbol: {
+         type: "simple-line",
+         style: "short-dot",
+         width: "1px",
+         color: 'green'
+      }
+   }
+
+   var bikePaths = new FeatureLayer({
+      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+      renderer: bikeTrailsRenderingRule,
+      definitionExpress: 'USE_BIKE = "YES"'
+   })
+   
+   map.add(bikePaths)
+
+
+
+
+
+   const createFillSymbols = (value, color) => ({
+      value,
+      symbol: {
+         color,
+         type: "simple-fill",
+         style: "solid",
+         outline: {
+            style: "none"
+         }
+      },
+      label: value
+   })
+
+   const openSpacesRenderingRule = {
+      type: "unique-value",
+      field: "TYPE",
+      uniqueValueInfos: [
+         createFillSymbols("Natural Areas", "#9E559C"),
+         createFillSymbols("Regional Open Space", "#A7C636"),
+         createFillSymbols("Local Park", "#149ECE"),
+         createFillSymbols("Regional Recreational Park", "#ED5151")
+      ]
+   }
+
+   const openspacesLayer = new FeatureLayer({
+      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0",
+      renderer: openSpacesRenderingRule,
+      opacity: 0.4
+   })
+
+   map.add(openspacesLayer)
 
 })
