10d7976aa9a316511134d08105c186d262f4f040
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-08-28 16:53:43 -0700

    arcgis api - global covid timeslider

--- ArcGIS API/MyMaps/COVID19_Global/index.js
+++ ArcGIS API/MyMaps/COVID19_Global/index.js
@@ -2,8 +2,9 @@ require([
    "esri/Map", 
    "esri/views/MapView",
    "esri/layers/TileLayer",
-   "esri/layers/FeatureLayer"
-], function(Map, MapView, TileLayer, FeatureLayer){
+   "esri/layers/FeatureLayer",
+   "esri/widgets/TimeSlider"
+], function(Map, MapView, TileLayer, FeatureLayer, TimeSlider){
 
    var fireflyLayer = new TileLayer({
       url: 'https://fly.maptiles.arcgis.com/arcgis/rest/services/World_Imagery_Firefly/MapServer'
@@ -14,7 +15,18 @@ require([
    })
 
    var covidLayer = new FeatureLayer({
-      url: 'https://services8.arcgis.com/bc1qjUYAgNrEBXVh/arcgis/rest/services/COVID19_Time_Series_Combined/FeatureServer'
+      url: 'https://services8.arcgis.com/bc1qjUYAgNrEBXVh/arcgis/rest/services/COVID19_Time_Series_Combined/FeatureServer',
+      renderer: {
+         type: "heatmap",
+         field: "Deaths",
+         colorStops: [
+           { ratio: 0, color: "rgba(255, 255, 255, 0)" },
+           { ratio: 0.5, color: "rgba(255, 140, 0, 1)" },
+           { ratio: 1, color: "rgba(255, 0, 0, 1)" }
+         ],
+         minPixelIntensity: 0,
+         maxPixelIntensity: 5000000
+      }
    })
 
    var map = new Map({
@@ -25,8 +37,63 @@ require([
    var view = new MapView({
       container: "viewDiv",
       center: [0, 20],
-      zoom: 3,
+      zoom: 2,
       map: map
+   })
+
+
+   var timeSlider = new TimeSlider({
+      container: "timeSliderDiv",
+      view: view,
+      // show data within a given time range
+      // in this case data within one year
+      mode: "time-window",
+      fullTimeExtent: {
+         start: new Date(2020, 0, 1),
+         end: new Date(2020, 11, 31) > new Date() ? new Date() : new Date(2020, 11, 31)
+      },
+      stops: {
+         interval: {
+           value: 10,
+           unit: "days"
+         }
+      },
+      tickConfigs: [{
+         mode: 'count',
+         values: new Date(2020, 11, 31) > new Date() ? (new Date()).getMonth() + 1 : 12,
+         labelsVisible: true,
+         labelFormatFunction: value => {
+            const date = new Date(value)
+            return date.toLocaleString('default', { month: 'short' } )
+         }
+      }]
+   });
+
+   view.ui.add(timeSlider, "bottom-right");
+
+   var covidLayerView;
+   view.whenLayerView(covidLayer)
+      .then(layerView => {
+
+         console.log(covidLayer.timeInfo)
+
+         covidLayerView = layerView
+         const fullTimeExtent = covidLayer.timeInfo.fullTimeExtent
+         const start = fullTimeExtent.start
+         // timeSlider.fullTimeExtent = fullTimeExtent
+         // timeSlider.values = [start]
+
+      })
+
+
+   timeSlider.watch("timeExtent", function(value){
+      // update layer view filter to reflect current timeExtent
+
+      console.log(value)
+
+      covidLayerView.filter = {
+         timeExtent: value
+      }
    })
 
 })
