0c2b1e060ef3cb90cb6399ad350102d2965fa91c
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-09-10 16:38:44 -0700

    arcgis api heatmap time series

--- ArcGIS API/MyMaps/COVID19_Global/index.js
+++ ArcGIS API/MyMaps/COVID19_Global/index.js
@@ -1,10 +1,10 @@
 require([
-   "esri/Map", 
+   "esri/Map",
    "esri/views/MapView",
-   "esri/layers/TileLayer",
    "esri/layers/FeatureLayer",
-   "esri/widgets/TimeSlider"
-], function(Map, MapView, TileLayer, FeatureLayer, TimeSlider){
+   "esri/layers/TileLayer",
+   "esri/widgets/TimeSlider",
+], function (Map, MapView, FeatureLayer, TileLayer, TimeSlider, heatmapRendererCreator, heatmapSchemes) {
 
    var fireflyLayer = new TileLayer({
       url: 'https://fly.maptiles.arcgis.com/arcgis/rest/services/World_Imagery_Firefly/MapServer'
@@ -15,120 +15,90 @@ require([
    })
 
    var covidLayer = new FeatureLayer({
-      url: 'https://services8.arcgis.com/bc1qjUYAgNrEBXVh/arcgis/rest/services/COVID19_Time_Series_Combined/FeatureServer',
-      outFields: ['*'],
+      url:
+         "https://services8.arcgis.com/bc1qjUYAgNrEBXVh/arcgis/rest/services/COVID19_Time_Series_Combined/FeatureServer",
+      outFields: ["*"],
       renderer: {
          type: "heatmap",
          field: "Confirmed",
          colorStops: [
-           { ratio: 0, color: "rgba(255, 255, 255, 0)" },
-           { ratio: 0.5, color: "rgba(255, 140, 0, 1)" },
-           { ratio: 1, color: "rgba(255, 0, 0, 1)" }
+            { ratio: 0, color: "rgba(255, 255, 255, 0)" },
+            { ratio: 0.025, color: "rgba(255, 255, 255, 0.7)" },
+            { ratio: 0.1, color: "rgba(255, 255, 0, 1)" },
+            { ratio: 0.25, color: "rgba(255, 140, 0, 1)" },
+            { ratio: 1, color: "rgba(255, 0, 0, 1)" }
          ],
-         minPixelIntensity: 0,
-         maxPixelIntensity: 5000000
+         minPixelIntensity: 1,
+         maxPixelIntensity: 100000000,
       },
-      // renderer: {
-      //    type: "simple", 
-      //    field: "Confirmed",
-      //    symbol: {
-      //       type: "simple-marker",
-      //       size: 6,
-      //       color: "black",
-      //       outline: {
-      //          width: 0.5,
-      //          color: "white"
-      //       }
-      //    },
-      //    visualVariables: [
-      //       {
-      //          type: 'size',
-      //          field: 'Confirmed',
-      //          stops: [
-      //             {
-      //               value: 1, 
-      //               size: 3 
-      //             },
-      //             {
-      //               value: 500, 
-      //               size: 10 
-      //             },
-      //             {
-      //                value: 100000, 
-      //                size: 30 
-      //              }
-      //          ]
-      //       }
-      //    ]
-      // },
-      popupTemplate: {
-         title: "{Date}"
-      }
-   })
+   });
 
+   // define map and view
    var map = new Map({
       layers: [fireflyLayer, covidLayer, referenceLayer]
-   })
+   });
 
    var view = new MapView({
       container: "viewDiv",
       center: [0, 20],
       zoom: 2,
-      map: map
-   })
-
+      map: map,
+      highlightOptions: {
+         fillOpacity: 0,
+         color: [50, 50, 50],
+      },
+   });
 
    var timeSlider = new TimeSlider({
       container: "timeSliderDiv",
       view: view,
       mode: "time-window",
       fullTimeExtent: {
-         start: new Date(2020, 0, 21),
-         end: new Date(2020, 11, 31) > new Date() ? new Date() : new Date(2020, 11, 31)
+         start: new Date(2020, 1, 1),
+         end: new Date(2020, 11, 1),
       },
+      playRate: 4000,
       stops: {
          interval: {
-           value: 10,
-           unit: "days"
-         }
+            value: 1,
+            unit: "months",
+         },
       },
-      tickConfigs: [{
-         mode: 'count',
-         values: new Date(2020, 11, 31) > new Date() ? (new Date()).getMonth() + 1 : 12,
-         labelsVisible: true,
-         labelFormatFunction: value => {
-            const date = new Date(value)
-            return date.toLocaleString('default', { month: 'short' } )
-         }
-      }]
+      tickConfigs: [
+        {
+          mode: "count",
+          values:
+            new Date(2020, 11, 31) > new Date() ? new Date().getMonth() + 1 : 12,
+          labelsVisible: true,
+          labelFormatFunction: (value) => {
+            const date = new Date(value);
+            console.log(date)
+            return date.toLocaleString("default", { month: "short" });
+          }
+        }
+      ]
    });
+   updateDefintionExpression(timeSlider.timeExtent);
 
    view.ui.add(timeSlider, "bottom-right");
 
-   var covidLayerView;
-   view.whenLayerView(covidLayer)
-      .then(layerView => {
+   timeSlider.watch("timeExtent", function (value) {
+      updateDefintionExpression(value);
+   });
 
-         console.log('timeinfo', covidLayer.timeInfo)
-         covidLayerView = layerView
+   function updateDefintionExpression(timeExtent) {
+      const start = toDateString(timeExtent.start);
+      const end = toDateString(timeExtent.end);
+      covidLayer.definitionExpression = `Date >= DATE '${start}' AND Date <= DATE '${end}'`;
+   }
 
-      })
-
-
-   timeSlider.watch("timeExtent", function(value){
-      // update layer view filter to reflect current timeExtent
-
-      console.log(value)
-      const start = value.start.getTime()
-      const end = value.end.getTime()
-
-      covidLayerView.effect = {
-         filter: {
-            timeExtent: value
-            // where: `Date >= '${start}' AND Date <= '${end}'`
-         },
-         excludedEffect: "grayscale(20%) opacity(12%)"
-      }  
-   })
-
-})
+   function toDateString(date) {
+      const y = `${date.getUTCFullYear()}`;
+      const m = `${date.getUTCMonth()}`.padStart(2, "0");
+      const d = `${date.getUTCDate()}`.padStart(2, "0");
+      const h = `${date.getUTCHours()}`.padStart(2, "0");
+      const mm = `${date.getUTCMinutes()}`.padStart(2, "0");
+      const s = `${date.getUTCSeconds()}`.padStart(2, "0");
+      return `${y}-${m}-${d} ${h}:${mm}:${s}`;
+   }
+});
