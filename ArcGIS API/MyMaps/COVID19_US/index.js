8a56d2bd4a35c7f39b50e222a448cdaaf1257590
slutske22 <seth.lutske@gmail.com>
2020-11-12 22:42:05 -0800

    us covid

--- ArcGIS API/MyMaps/COVID19_US/index.js
+++ ArcGIS API/MyMaps/COVID19_US/index.js
@@ -1,95 +1,89 @@
-import popupTemplate from './popupTemplate.js'
-import * as renderers from './renderers.js'
+import popupTemplate from './popupTemplate.js';
+import * as renderers from './renderers.js';
 
 // Color Ramps:
 // https://developers.arcgis.com/javascript/latest/guide/esri-color-ramps/index.html
 
 require([
-   "esri/Map", 
-   "esri/views/MapView",
-   "esri/layers/FeatureLayer",
-   "esri/widgets/Legend",
-   "esri/widgets/Expand",
-   "esri/smartMapping/statistics/classBreaks"
-], function(Map, MapView, FeatureLayer, Legend, Expand, classBreaks){
+	'esri/Map',
+	'esri/views/MapView',
+	'esri/layers/FeatureLayer',
+	'esri/widgets/Legend',
+	'esri/widgets/Expand',
+	'esri/smartMapping/statistics/classBreaks',
+], function (Map, MapView, FeatureLayer, Legend, Expand, classBreaks) {
+	var COVIDLayer = new FeatureLayer({
+		url:
+			'https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/US_County_COVID19_Trends/FeatureServer',
+		outFields: ['*'],
+		popupTemplate,
+	});
 
-   var COVIDLayer = new FeatureLayer({
-      url: "https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/US_County_COVID19_Trends/FeatureServer",
-      outFields: ["*"],
-      popupTemplate
-   })
+	var SocialDistancingScore = new FeatureLayer({
+		url:
+			'https://services.arcgis.com/DO4gTjwJVIJ7O9Ca/arcgis/rest/services/Unacast_Latest_Available__Visitation_and_Distance_/FeatureServer',
+		outFields: ['*'],
+	});
 
-   var map = new Map({
-      basemap: "gray-vector",
-      layers: [COVIDLayer]
-   })
+	var map = new Map({
+		basemap: 'gray-vector',
+		layers: [COVIDLayer, SocialDistancingScore],
+	});
 
-   var view = new MapView({
-      container: "viewDiv",
-      center: [-100, 38],
-      zoom: 4,
-      map: map
-   })
+	var view = new MapView({
+		container: 'viewDiv',
+		center: [-100, 38],
+		zoom: 4,
+		map: map,
+	});
 
+	function changeField(e) {
+		const field = e.target.getAttribute('data-field');
 
+		classBreaks({
+			layer: COVIDLayer,
+			field,
+			numClasses: 5,
+		}).then(function (response) {
+			const { maxValue } = response;
+			COVIDLayer.renderer = renderers[field](0, maxValue);
+		});
+	}
 
-   function changeField(e){
+	const fieldNodes = document.querySelectorAll(`.field-item`);
+	const fieldElement = document.getElementById('field-filter');
+	fieldElement.addEventListener('click', changeField);
 
-      const field = e.target.getAttribute('data-field')
+	var fieldExpand = new Expand({
+		view,
+		content: fieldElement,
+		expandIconClass: 'esri-icon-filter',
+		group: 'top-left',
+		expanded: true,
+	});
 
-      classBreaks({
-         layer: COVIDLayer,
-         field,
-         numClasses: 5
-      })
-      .then(function(response){
-         const { maxValue } = response;
-         COVIDLayer.renderer = renderers[field](0, maxValue)
-      })
+	var legend = new Legend({
+		view: view,
+		layerInfos: [
+			{
+				layer: COVIDLayer,
+			},
+		],
+	});
 
-   }
+	view.ui.add(legend, 'top-right');
+	view.ui.add(fieldExpand, 'top-left');
 
-   const fieldNodes = document.querySelectorAll(`.field-item`);
-   const fieldElement = document.getElementById("field-filter");
-   fieldElement.addEventListener('click', changeField)
+	classBreaks({
+		layer: COVIDLayer,
+		field: 'totCases',
+		numClasses: 5,
+	}).then(function (response) {
+		const { maxValue } = response;
+		COVIDLayer.renderer = renderers.totCases(0, maxValue);
+	});
 
-   var fieldExpand = new Expand({
-      view,
-      content: fieldElement,
-      expandIconClass: "esri-icon-filter",
-      group: "top-left",
-      expanded: true
-   })
-
-   var legend = new Legend({
-      view: view,
-      layerInfos: [
-         {
-            layer: COVIDLayer,
-         }
-      ]
-   })
-
-   view.ui.add(legend, 'top-right')
-   view.ui.add(fieldExpand, 'top-left')
-
-
-   classBreaks({
-      layer: COVIDLayer,
-      field: "totCases",
-      numClasses: 5
-   })
-   .then(function(response){
-      const { maxValue } = response;
-      COVIDLayer.renderer = renderers.totCases(0, maxValue)
-   })
-
-
-   view.whenLayerView(COVIDLayer)
-      .then(() => {
-         fieldElement.style.display = 'block'
-      })
-
-})
-
-
+	view.whenLayerView(COVIDLayer).then(() => {
+		fieldElement.style.display = 'block';
+	});
+});
