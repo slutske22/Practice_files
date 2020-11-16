a069a1f0fb994970000f9253b55aa75ed2af468d
Seth <seth.lutske@gmail.com>
2020-11-16 15:26:04 -0800

    Update index.js

--- ArcGIS API/MyMaps/COVID19_US_swipe/index.js
+++ ArcGIS API/MyMaps/COVID19_US_swipe/index.js
@@ -1,96 +1,90 @@
-import popupTemplate from './popupTemplate.js'
-import * as renderers from './renderers.js'
+import popupTemplate from './popupTemplate.js';
+import * as renderers from './renderers.js';
 
 // Color Ramps:
 // https://developers.arcgis.com/javascript/latest/guide/esri-color-ramps/index.html
 
 require([
-   "esri/Map", 
-   "esri/views/MapView",
-   "esri/layers/FeatureLayer",
-   "esri/widgets/Legend",
-   "esri/widgets/Expand",
-   "esri/widgets/Swipe",
-   "esri/smartMapping/statistics/classBreaks",
-], function(Map, MapView, FeatureLayer, Legend, Expand, Swipe, classBreaks){
+	'esri/Map',
+	'esri/views/MapView',
+	'esri/layers/FeatureLayer',
+	'esri/widgets/Legend',
+	'esri/popup/content/MediaContent',
+	'esri/popup/content/support/ChartMediaInfoValue',
+	'esri/popup/content/LineChartMediaInfo',
+], function (
+	Map,
+	MapView,
+	FeatureLayer,
+	Legend,
+	MediaContent,
+	ChartMediaInfoValue,
+	LineChartMediaInfo
+) {
+	var unemploymentLayer = new FeatureLayer({
+		url:
+			'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/BLS_Monthly_Unemployment_Current_14_Months/FeatureServer/2',
+		outFields: ['*'],
+		popupTemplate: {
+			content: [
+				{
+					type: 'text',
+					text: '<h2>BLS Unemployment Statistics, past 14 months<h2>',
+				},
+				{
+					type: 'media',
+					mediaInfos: [
+						{
+							type: 'line-chart',
+							title: 'Unemployment',
+							caption: 'BLS unemployment data for the last 14 months',
+							value: {
+								fields: [
+									'PctUnemployed_CurrentMonth',
+									'PctUnemployed_01Month',
+									'PctUnemployed_02Month',
+									'PctUnemployed_03Month',
+									'PctUnemployed_04Month',
+									'PctUnemployed_05Month',
+									'PctUnemployed_06Month',
+									'PctUnemployed_07Month',
+									'PctUnemployed_08Month',
+									'PctUnemployed_09Month',
+									'PctUnemployed_10Month',
+									'PctUnemployed_11Month',
+									'PctUnemployed_12Month',
+									'PctUnemployed_13Month',
+								],
+								normalizeField: null,
+								// tooltipField: '<field name>',
+							},
+						},
+					],
+				},
+			],
+		},
+	});
 
-   var COVIDLayer = new FeatureLayer({
-      url: "https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/US_County_COVID19_Trends/FeatureServer",
-      outFields: ["*"],
-      popupTemplate
-   })
+	var map = new Map({
+		basemap: 'gray-vector',
+		layers: [unemploymentLayer],
+	});
 
-   var map = new Map({
-      basemap: "gray-vector",
-      layers: [COVIDLayer]
-   })
+	var view = new MapView({
+		container: 'viewDiv',
+		center: [-100, 38],
+		zoom: 4,
+		map: map,
+	});
 
-   var view = new MapView({
-      container: "viewDiv",
-      center: [-100, 38],
-      zoom: 4,
-      map: map
-   })
+	var legend = new Legend({
+		view: view,
+		layerInfos: [
+			{
+				layer: unemploymentLayer,
+			},
+		],
+	});
 
-
-
-   function changeField(e){
-
-      const field = e.target.getAttribute('data-field')
-
-      classBreaks({
-         layer: COVIDLayer,
-         field,
-         numClasses: 5
-      })
-      .then(function(response){
-         const { maxValue } = response;
-         COVIDLayer.renderer = renderers[field](0, maxValue)
-      })
-
-   }
-
-   const fieldNodes = document.querySelectorAll(`.field-item`);
-   const fieldElement = document.getElementById("field-filter");
-   fieldElement.addEventListener('click', changeField)
-
-   var fieldExpand = new Expand({
-      view,
-      content: fieldElement,
-      expandIconClass: "esri-icon-filter",
-      group: "top-left",
-      expanded: true
-   })
-
-   var legend = new Legend({
-      view: view,
-      layerInfos: [
-         {
-            layer: COVIDLayer,
-         }
-      ]
-   })
-
-   view.ui.add(legend, 'top-right')
-   view.ui.add(fieldExpand, 'top-left')
-
-
-   classBreaks({
-      layer: COVIDLayer,
-      field: "totCases",
-      numClasses: 5
-   })
-   .then(function(response){
-      const { maxValue } = response;
-      COVIDLayer.renderer = renderers.totCases(0, maxValue)
-   })
-
-
-   view.whenLayerView(COVIDLayer)
-      .then(() => {
-         fieldElement.style.display = 'block'
-      })
-
-})
-
-
+	view.ui.add(legend, 'top-right');
+});
