f2a55149684bc1c0d99dbc8ef68145d4d7dafbd7
slutske22 <seth.lutske@gmail.com>
2020-12-14 18:04:05 -0800

    arcgis js api hurriances

--- ArcGIS API/MyMaps/hurricanes/index.js
+++ ArcGIS API/MyMaps/hurricanes/index.js
@@ -1,3 +1,5 @@
+import * as renderers from './renderers.js';
+
 require([
 	'esri/Map',
 	'esri/views/MapView',
@@ -14,14 +16,27 @@ require([
 			'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Reference/MapServer',
 	});
 
-	const hurricanePositions = new FeatureLayer({
+	const hurricanePositionsSwirls = new FeatureLayer({
 		url:
-			'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Recent_Hurricanes_v1/FeatureServer',
+			'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Recent_Hurricanes_v1/FeatureServer/0',
+		renderer: renderers.blueSwirlRenderer,
+		// minScale: 0,
+		// maxScale: 26601363,
+	});
+
+	const hurricanePositionsDots = new FeatureLayer({
+		url:
+			'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Recent_Hurricanes_v1/FeatureServer/0',
+		renderer: renderers.blueDotRenderer,
+		// minScale: 32295367,
+		// maxScale: 0,
+		definitionExpression:
+			'(INTENSITY = 5) OR (INTENSITY = 10) OR (INTENSITY = 15) OR (INTENSITY = 20) OR (INTENSITY = 25) OR (INTENSITY = 30) OR (INTENSITY = 35) OR (INTENSITY = 40) OR (INTENSITY = 45) OR (INTENSITY = 50) OR (INTENSITY = 55) OR (INTENSITY = 60)',
 	});
 
 	var map = new Map({
 		basemap: 'dark-gray-vector',
-		layers: [hurricanePositions],
+		layers: [hurricanePositionsSwirls, hurricanePositionsDots],
 	});
 
 	var view = new MapView({
