f35a25d02d4ed9c3d14a21a2693872e34266a8e3
slutske22 <seth.lutske@gmail.com>
2021-01-02 22:56:05 -0800

    node image processing

--- node_image_processing/server.js
+++ node_image_processing/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import bp from 'body-parser';
 import dotenv from 'dotenv';
-import { createCanvas, loadImage, createImageData } from 'canvas';
+import { createCanvas, loadImage } from 'canvas';
 
 dotenv.config();
 
-const MAPBOX_RGB_URL = `https://api.mapbox.com/v4/mapbox.terrain-rgb/13/543/3615.pngraw?access_token=${process.env.MAPBOX_TOKEN}`;
+const MAPBOX_RGB_URL = (coord) =>
+	`https://api.mapbox.com/v4/mapbox.terrain-rgb/13/543/${coord}.pngraw?access_token=${process.env.MAPBOX_TOKEN}`;
 
 const port = process.env.PORT || 3232;
 const app = express();
@@ -20,16 +21,32 @@ app.use(bp.urlencoded({ extended: true }));
 // 	res.send({});
 // });
 
+const tiles = {};
+const boundsCoords = [3615, 3616, 3617, 3618];
+
 async function loadTile() {
 	const canvas = createCanvas(256, 256);
 	const ctx = canvas.getContext('2d');
-	loadImage(MAPBOX_RGB_URL)
-		.then((img) => {
-			ctx.drawImage(img, 0, 0, 256, 256);
-			const imageData = ctx.getImageData(0, 0, 256, 256);
-			console.log(imageData);
+
+	// loadImage(MAPBOX_RGB_URL(3615))
+	// 	.then((img) => {
+	// 		ctx.drawImage(img, 0, 0, 256, 256);
+	// 		const imageData = ctx.getImageData(0, 0, 256, 256);
+	// 		console.log(imageData);
+	// 	})
+	// 	.catch((e) => console.log(e));
+
+	Promise.all(boundsCoords.map((coord) => loadImage(MAPBOX_RGB_URL(coord))))
+		.then((images) => {
+			images.forEach((img, ind) => {
+				const canvas = createCanvas(256, 256);
+				const ctx = canvas.getContext('2d');
+				ctx.drawImage(img, 0, 0, 256, 256);
+				const imageData = ctx.getImageData(0, 0, 256, 256);
+				tiles[boundsCoords[ind]] = imageData;
+			});
 		})
-		.catch((e) => console.log(e));
+		.then(() => console.log(tiles));
 }
 
 loadTile();
