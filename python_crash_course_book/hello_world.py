d0eb15c5c9401eda84c1bafc83a1173cba2c1930
slutske22 <seth.lutske@gmail.com>
2021-03-13 20:51:37 -0800

    Python fun

---
+++ python_crash_course_book/hello_world.py
@@ -0,0 +1,12 @@
+# %%
+print("\n\nWelcome to your 'Hello World' file!")
+
+# %%
+name = "Blue Ohana"
+print(f"Hello, {name}\n\n")
+
+# %%
+message = "       This is a message who's white space was stripped  "
+print(message.strip())
+
+# %%
