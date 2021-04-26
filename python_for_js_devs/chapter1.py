dbbb30495afd9fcfbb6c91ac259f3bf9bbdef2de
slutske22 <seth.lutske@gmail.com>
2021-04-26 14:27:04 -0700

    More python fun

--- python_for_js_devs/chapter1.py
+++ python_for_js_devs/chapter1.py
@@ -35,4 +35,50 @@ a, b = 0, 1
 while b < 1000:
     print(b, end=' ', flush=True)
     a, b = b, a + b
+# %% Functions always return something, even if its none
+
+
+def do_me_something(n=1):
+    print(n)
+
+
+return_value = do_me_something(5)
+print(return_value)
+# %%  Ternaries
+money = 10_000
+
+action = "Party like its 1999" if money > 1000 else "Get a j-o-b foo"
+
+print(action)
+
+# %%  Bitwise operators
+
+a = 0x0a
+b = 0x02
+bb = 0x0f
+c = a & b  # result is bits that exist in both a and b
+d = a | b  # result is bits that exist in both a or b
+e = a ^ bb  # only if one OR the other have a bit
+f = a << b
+g = a >> b
+
+
+def print_the_result(arg1, name1, arg2, name2, result, resultname):
+    print(
+        f'hex: {name1} is {arg1:02x}, {name2} is {arg2:02x}, {resultname} is {result:02x}')
+    print(
+        f'bin: {name1} is {arg1:08b}, \n     {name2} is {arg2:08b}, \n     {resultname} is {result:08b}')
+
+
+print_the_result(a, 'a', b, 'b', c, 'c')
+print()
+print_the_result(a, 'a', b, 'b', d, 'd')
+print()
+print_the_result(a, 'a', bb, 'bb', e, 'e')
+print()
+print_the_result(a, 'a', b, 'b', f, 'f')
+print()
+print_the_result(a, 'a', b, 'b', g, 'g')
+# %%
+print(__name__)
 # %%
---
+++ python_for_js_devs/functions.py
@@ -0,0 +1,13 @@
+# %%
+def main():
+    kitten(5)
+
+
+def kitten(a):
+    print("Meow.")
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
+# %%
