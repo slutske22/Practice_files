3012e6bb7020389ce3f36ecd8669c4584b9dbb11
slutske22 <seth.lutske@gmail.com>
2021-05-31 21:02:44 -0700

    More C

--- c_ess/ch2.c
+++ c_ess/ch2.c
@@ -4,9 +4,15 @@
 /* Generate multiple lines of output */
 int main() {
 
+  int rows;
+
   /* Process input */
+  printf("Enter a value between 0 and 18: ");
+  scanf("%d", &rows);
   /* Avoid out of range values */
+
   /* process rows */
+  printf("The computer will process %i rows\n", rows);
 
   return 0;
 }
