55fcb6e70595ce60da52a86ad55ca8221aea49dd
Seth Lutske <seth.lutske@gmail.com>
2021-06-07 07:58:04 -0700

    Leaflet nighttime time sliders

--- Leaflet_Experiments/nighttime/src/index.js
+++ Leaflet_Experiments/nighttime/src/index.js
@@ -1,9 +1,10 @@
 import terminator from '@joergdietrich/leaflet.terminator';
 import 'leaflet-boundary-canvas';
+import './utils';
 
 // Define some maps options
 var mapOptions = {
-	center: [20.27, -157],
+	center: [0, 0],
 	zoom: 2,
 };
 
@@ -23,13 +24,16 @@ var Stadia_AlidadeSmooth = L.tileLayer(
 Stadia_AlidadeSmooth.addTo(map);
 
 // Create GeoJSON of nighttime based on leaflet.terminator:
-const nightmask = terminator().toGeoJSON();
+const nightmask = terminator().addTo(map);
+
+const nightmaskJson = nightmask.toGeoJSON();
 
 // Use nighttime mast geojson as boundary for a boundarycanvas:
 const nightraster = L.TileLayer.boundaryCanvas(
 	'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
 	{
-		boundary: nightmask,
+		boundary: nightmaskJson,
+		trackAttribution: true,
 		maxZoom: 20,
 		attribution:
 			'&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
@@ -37,3 +41,54 @@ const nightraster = L.TileLayer.boundaryCanvas(
 );
 
 nightraster.addTo(map);
+
+const updateMapTime = (time) => {
+	nightmask.setTime(time);
+
+	nightraster.options.boundary = nightmask.toGeoJSON();
+};
+
+/** Time slider stuff */
+
+// Grab dom nodes:
+const daySlider = document.getElementById('day-slider');
+const dayReadout = document.getElementById('day-readout');
+const timeSlider = document.getElementById('time-slider');
+const timeReadout = document.getElementById('time-readout');
+
+// Get now date
+const now = new Date();
+const timeNow = now.getHours() * 60 + now.getMinutes();
+
+// Set sliders to right now
+timeSlider.value = timeNow;
+timeReadout.innerHTML = now.toLocaleTimeString();
+
+if (now.isLeapYear()) {
+	daySlider.max = 366;
+}
+daySlider.value = now.getDOY();
+dayReadout.innerHTML = now.toLocaleDateString();
+
+// Listen to sliders events and adjust time object
+var time = new Date(now.getTime());
+
+timeSlider.addEventListener('input', (e) => {
+	const { value } = e.target;
+	time.setHours(value / 60);
+	time.setMinutes(value % 60);
+	updateMapTime(time.getTime());
+	timeReadout.innerHTML = time.toLocaleTimeString();
+});
+
+daySlider.addEventListener('input', (e) => {
+	const { value } = e.target;
+
+	const timeWas = new Date(time.getTime());
+	time = new Date(now.getFullYear(), 0);
+	time.setDate(value);
+	time.setHours(timeWas.getHours());
+	time.setMinutes(timeWas.getMinutes());
+	updateMapTime(time.getTime());
+	dayReadout.innerHTML = time.toLocaleDateString();
+});
