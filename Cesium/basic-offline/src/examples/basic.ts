708ed57257d1ea78a4c7921f6106b453f6806742
Seth Lutske <seth.lutske@gmail.com>
2023-06-09 07:05:13 -1000

    Various cesium examples

---
+++ Cesium/basic-offline/src/examples/basic.ts
@@ -0,0 +1,29 @@
+// This is an example of using Cesium "Offline", meaning disconnected from the
+// external Internet.  It must still be served from a local web server, but
+// does not rely on any outside resources or services.  For more info, see:
+// https://github.com/CesiumGS/cesium/wiki/Offline-Guide
+
+import * as Cesium from 'cesium';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+// @ts-expect-error required by cesium offline
+window.CESIUM_BASE_URL = 'http://localhost:5000/Cesium';
+
+const viewer = new Cesium.Viewer('cesiumContainer', {
+	// @ts-expect-error because ts is wrong here?
+	baseLayer: Cesium.ImageryLayer.fromProviderAsync(
+		// Need to set this to avoid cesium using the default service
+		// which requires their api
+		Cesium.TileMapServiceImageryProvider.fromUrl(
+			Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+		)
+	),
+	baseLayerPicker: false,
+	geocoder: false,
+});
+
+const osm = new Cesium.OpenStreetMapImageryProvider({
+	url: 'http://localhost:5000/world',
+});
+
+viewer.scene.imageryLayers.addImageryProvider(osm);
