fdb0bd1029b130ccd243eb76b712bb97df1c70c6
Seth Lutske <sethlutske@BlueOhanaBeast.lan>
2023-06-20 14:09:54 -1000

    Adding simple draw example

--- webgl/playground/src/example6-mouse-events/index.ts
+++ webgl/playground/src/example6-mouse-events/index.ts
@@ -5,27 +5,29 @@ import { getGlContext, setup } from '../utils';
 import vert1 from './vert.vert';
 import frag1 from './frag.frag';
 
-const canvas = document.getElementById('canvas');
-const gl = getGlContext('canvas', { preserveDrawingBuffer: true });
+document.addEventListener('DOMContentLoaded', () => {
+	const canvas = document.getElementById('canvas');
+	const gl = getGlContext('canvas', { preserveDrawingBuffer: true });
 
-const { program } = setup(gl, vert1, frag1);
+	const { program } = setup(gl, vert1, frag1);
 
-gl.useProgram(program);
+	gl.useProgram(program);
 
-// disable position attribute
-const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-// gl.disableVertexAttribArray(positionAttributeLocation);
+	// disable position attribute
+	const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+	// gl.disableVertexAttribArray(positionAttributeLocation);
 
-// set the resolution
-const resolutionUniformLocation = gl.getUniformLocation(
-	program,
-	'u_resolution'
-);
-gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+	// set the resolution
+	const resolutionUniformLocation = gl.getUniformLocation(
+		program,
+		'u_resolution'
+	);
+	gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-canvas.addEventListener('mousemove', (e) => {
-	const x = e.x - canvas.offsetLeft + 1;
-	const y = e.y - canvas.offsetTop + 1;
-	gl.vertexAttrib2f(positionAttributeLocation, x, y);
-	gl.drawArrays(gl.POINTS, 0, 1);
+	canvas.addEventListener('mousemove', (e) => {
+		const x = e.x - canvas.offsetLeft + 1;
+		const y = e.y - canvas.offsetTop + 1;
+		gl.vertexAttrib2f(positionAttributeLocation, x, y);
+		gl.drawArrays(gl.POINTS, 0, 1);
+	});
 });
