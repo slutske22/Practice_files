3825bb2a193bd5b25d78a424020f9d4ae1ebabe4
Seth Lutske <Seth@Erics-MBP-2.lan>
2025-02-26 11:04:45 -1000

    Swift learning project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Seth Lutske on 2/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive Text Goes Here.")
+            }
+            .padding()
+        }
+        
+        Spacer()
+            
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Seth Lutske on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
