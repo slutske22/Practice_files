dfab6191b74b74984655339a6e2410fa770e342b
Seth Lutske <Seth@Erics-MacBook-Pro-2.local>
2025-02-28 11:35:35 -1000

    Moving swift files

---
+++ Landmarks/Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Seth Lutske on 2/28/25.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
