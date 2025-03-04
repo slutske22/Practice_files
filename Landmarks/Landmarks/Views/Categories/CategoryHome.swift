70658faece8b958c0416584098ae049555bf9d15
Seth Lutske <Seth@MacBookPro.lan>
2025-03-04 10:33:18 -1000

    More swift

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Seth Lutske on 3/4/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                        Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
